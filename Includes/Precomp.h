50520def5058e460501c2798965dbbe74885259d
Konstantinos Melissaratos <kmel@mikroskosmos.gr>
2025-02-14 14:08:40 +0200

    Initial commit

---
+++ Includes/Precomp.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef K_MEANS_PRECOMP_H
+#define K_MEANS_PRECOMP_H
+
+// Std includes
+#include <iostream>
+#include <cmath>
+
+// nanoflann
+#include <nanoflann.hpp>
+
+#endif //K_MEANS_PRECOMP_H
